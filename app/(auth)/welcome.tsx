716191e6d185ce104f5ca324db557ccb60675677
dev@thevoiceactorstudio.com <dev@thevoiceactorstudio.com>
2025-01-10 09:49:57 -0300

    feat: contants

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,12 +1,37 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
 
 const Welcome = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
-    <SafeAreaView>
-      <View className="flex text-center justify-center">
-        <Text className="color-red-600">Welcome</Text>
-        <Text>Welcome to the Welcome screen!</Text>
-      </View>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        [{}]
+      </Swiper>
     </SafeAreaView>
   );
 };
---
+++ constants/index.ts
@@ -0,0 +1,20 @@
+export const onboarding = [
+    {
+        id: 1,
+        title: "The perfect ride is just a tap away!",
+        description: "The journey begins with Ryde",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 1,
+        title: "Welcome to the app",
+        description: "This is a description",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 1,
+        title: "Welcome to the app",
+        description: "This is a description",
+        image: "https://via.placeholder.com/150",
+    },
+]
